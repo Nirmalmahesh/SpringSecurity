c584de35ccf0656dc61c370ff752a63a5bd3994c
NIRMALMAHESH <NIRMALMAHESH@LAPTOP-SDVFL5D9>
2019-04-20 23:05:48 +0530

    Bcrypt Encoder

--- SpringSecurity/src/main/java/com/example/security/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/example/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
@@ -27,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public AuthenticationProvider authProvider()
 	{
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
 		provider.setUserDetailsService(service);
 		return provider;
 	}
