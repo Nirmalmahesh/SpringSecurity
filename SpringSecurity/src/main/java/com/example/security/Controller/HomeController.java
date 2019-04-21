70fc184eac2553e6a6d82fff4524f40df053c277
NIRMALMAHESH <NIRMALMAHESH@LAPTOP-SDVFL5D9>
2019-04-21 07:05:55 +0530

    With Google Oauth

--- SpringSecurity/src/main/java/com/example/security/Controller/HomeController.java
+++ SpringSecurity/src/main/java/com/example/security/Controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-	@GetMapping("/")
+	@GetMapping("/home")
 	public String Home()
 	{
 		return "I am On Home";
--- SpringSecurity/src/main/java/com/example/security/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/example/security/SecurityConfig.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,17 +22,18 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
+@EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	UserDetailsService service;
-	@Bean
-	public AuthenticationProvider authProvider()
-	{
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(new BCryptPasswordEncoder());
-		provider.setUserDetailsService(service);
-		return provider;
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+			http
+				.csrf().disable()
+				.authorizeRequests().antMatchers("/login").permitAll()
+				.anyRequest().authenticated();
 	}
+
+	
 	
 }
